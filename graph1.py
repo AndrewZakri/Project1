68b469c52a338099025008a416efe13120b272b3
AndrewZakri <zakria@sunypoly.edu>
2025-06-17 15:59:41 -0400

    graph1.py

--- graph1.py
+++ graph1.py
@@ -2,13 +2,13 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 
-#Title of the app
+# Title of the app
 st.title("Flight Data Dashboard")
 
-#Flight data:Passengers
+# Flight data:Passengers
 df=pd.read_csv("passengers_yearly.csv")
 
-#Create a bar chart
+# Create a bar chart
 fig1=px.bar(df, x="Year", y="Domestic", title="Total Passenger Domestic Flights")
 
 fig2=px.bar(df, x="Year", y="International", title="Total Passenger International Flights")
@@ -16,7 +16,7 @@ fig2=px.bar(df, x="Year", y="International", title="Total Passenger Internationa
 fig3=px.bar(df, x="Year", y="Total", title="Total Passenger Flights")
 
 # Arrange plots: grid layout
-col1, col2, col3 =st.columns(3) #Create 3 columns
+col1, col2 =st.columns(2) #Create 2 columns
 
 with col1:
   st.plotly_chart(fig1, use_container_width=True)
@@ -24,5 +24,5 @@ with col1:
 with col2:
   st.plotly_chart(fig2, use_container_width=True)
 
-with col3:
-  st.plotly_chart(fig3, use_container_width=True)
+# Third plot is full-width
+st.plotly_chart(fig3, use_container_width=True)
