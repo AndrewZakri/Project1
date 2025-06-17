45613cb33e6ababcfe04f538562e53d91bdd99ee
AndrewZakri <zakria@sunypoly.edu>
2025-06-17 14:03:42 -0400

    graph1.py

--- graph1.py
+++ graph1.py
@@ -6,10 +6,10 @@ import pandas as pd
 st.title("Flight Data Dashboard")
 
 #Flight data:Passengers
-df=pd.read_csv('Passengers.csv')
+df=pd.read_csv('passengers.csv')
 
 #Create a bar chart
-fig=px.bar(df, x="Year", y="DOMESTIC", title="Total Passengers Domestic Flights")
+fig=px.bar(df, x="Year", y="Domestic", title="Total Passengers Domestic Flights")
 
 #Display the Plotly chart in Streamlit
 st.plotly_chart(fig)
